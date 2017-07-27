c82dd779755d151eecd165c510d41d7ef4b08a79
Soham Kulkarni <sohamk97@gmail.com>
2017-07-27 09:40:54 +0530

    Update webcam.py

--- Laughter Detection/webcam.py
+++ Laughter Detection/webcam.py
@@ -1,3 +1,16 @@
 # code to trigger webcam
 import np as numpy
 import cv2
+
+cap = cv2.VideoCapture(0)
+
+while (true):
+  _, frame = cap.read()
+  cv2.imshow('Live', frame)
+  
+  if cv2.waitKey(1)==0:
+    break;
+    
+cap.release()
+cv2.destroyAllWindows()
+  
