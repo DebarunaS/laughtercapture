7a329c7135a81ac57d14ef126fc603fff3548c4e
Soham Kulkarni <sohamk97@gmail.com>
2017-08-03 17:20:45 +0530

    Add files via upload

--- Laughter Detection/webcam.py
+++ Laughter Detection/webcam.py
@@ -6,11 +6,18 @@ import cv2
 
 # open default camera
 cap = cv2.VideoCapture(0)
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1080)
+cap.VideoCapture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH , 1024)
+
+# prepare VideoWriter
+out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 20.0, (640,480))
+
 
 # run loop for each frame until 'Esc' pressed
-while (true):
-  #get frame and display
+while (cap.isOpened()):
+  #get frame, save and display
   _, frame = cap.read()
+  out.write(frame)
   cv2.imshow('Live', frame)
   
   if cv2.waitKey(1)==27:
@@ -18,4 +25,5 @@ while (true):
 
 # release camera and destroy display window
 cap.release()
+out.release()
 cv2.destroyAllWindows()
